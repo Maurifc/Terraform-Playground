45230042279706b90b733655fcdb8f23447c4af6
maurifcarmo <mauri_vrb@hotmail.com>
2022-02-10 13:35:36 -0300

    add vm

--- main.tf
+++ main.tf
@@ -15,6 +15,27 @@ provider "google" {
   zone    = "us-east1-d"
 }
 
+# VPC
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+# VM
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name # VPC Binding
+    access_config { # Creates an external IP address
+    }
+  }
+}
+
