1d9e8ca7e1442836321fcd01b78a54276abaf2b3
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-05 22:48:22 -0300

    add bastion project
    - Create a vpc
    - Create three subnets
    - Create bastion host
    - Create vm to be accessed through bastion
    - Create router + cloud nat to allow private network access internet

---
+++ gcp_bastion/subnets.tf
@@ -0,0 +1,20 @@
+resource "google_compute_subnetwork" "subnet_a" {
+  name          = "sn-a"
+  region        = "us-east1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_subnetwork" "subnet_b" {
+  name          = "sn-b"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.2.0/24"
+}
+
+resource "google_compute_subnetwork" "subnet_c" {
+  name          = "sn-c"
+  region        = "us-west1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.4.0/24"
+}
